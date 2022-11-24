3dcf2558074b18894cff9b86c9c5f2ee707da6c7
Pablo Vieira <pablovieira.con@gmail.com>
2022-11-24 16:52:44 +1100

    wip on graphs

--- frontend/src/app/modules/companies/CompanyOverviewPage.tsx
+++ frontend/src/app/modules/companies/CompanyOverviewPage.tsx
@@ -1,12 +1,22 @@
 import {IMCard} from "../../../_investingmate/helpers";
 import {CompaniesHeader} from "./components/header/CompaniesHeader";
+// import TradingViewWidget from "react-tradingview-widget";
 
 const CompanyOverviewPage = () => {
   return (
     <>
-      <IMCard>
+      <IMCard className="pb-5 table-responsive">
         <CompaniesHeader />
-        OVERVIEW
+        {/*<TradingViewWidget*/}
+        {/*  symbol="NASDAQ:AAPL"*/}
+        {/*  timezone="Australia/Sydney"*/}
+        {/*  withdateranges*/}
+        {/*  details*/}
+        {/*  calendar*/}
+        {/*  show_popup_button*/}
+        {/*  popup_width={window.screen.width * 0.9 ?? "1000"}*/}
+        {/*  popup_height={window.screen.height * 0.8 ?? "650"}*/}
+        {/*/>*/}
       </IMCard>
     </>
   )
